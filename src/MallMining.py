2a2e6e91d823a7b525301dd467fa97ea390dab1c
GavDotZip <gavdotzip@gmail.com>
2024-03-09 17:05:42 +0000

    clean

--- src/MallMining.py
+++ src/MallMining.py
@@ -11,7 +11,7 @@ df = pd.read_csv('C:/Users/gavin/PycharmProjects/ConsumerBehaviour/src/data/Mall
 # Count the occurrences of each gender
 gender_counts = df['Gender'].value_counts()
 
-# Plot the bar chart
+# Plot the bar chart for Count of Gender
 plt.bar(gender_counts.index, gender_counts.values, color=['blue', 'red'])
 plt.xlabel('Gender')
 plt.ylabel('Count')
@@ -19,9 +19,10 @@ plt.title('Count of Each Gender')
 plt.grid(axis='y')
 plt.show()
 
-# plt.figure(figsize=(11, 8))
-# sns.lineplot(data=df[['Age', 'Annual Income (k$)', 'Spending Score (1-100)']], palette='magma')
-# plt.title('Line Plot of Age, Annual Income, and Spending Score')
-# plt.xlabel('Data Points')
-# plt.ylabel('Values')
-# plt.show()
+# Plotthe line chart for Age, Annual Income and Spending Score
+plt.figure(figsize=(11, 8))
+sns.lineplot(data=df[['Age', 'Annual Income (k$)', 'Spending Score (1-100)']], palette='magma')
+plt.title('Line Plot of Age, Annual Income, and Spending Score')
+plt.xlabel('Data Points')
+plt.ylabel('Values')
+plt.show()
