51dfa29685b10cb91a30aa8d7753271c4d954ceb
GavDotZip <gavdotzip@gmail.com>
2024-03-11 19:14:39 +0000

    created file

---
+++ src/ShoppingBehaviours.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the data
+df = pd.read_csv('C:/Users/gavin/PycharmProjects/ConsumerBehaviour/src/data/shopping_behavior_updated.csv')
+
+df.head()
