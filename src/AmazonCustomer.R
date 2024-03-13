6a0670c2d569e6665b6420cea71fb72d6824e2f8
GavDotZip <gavdotzip@gmail.com>
2024-03-13 18:28:42 +0000

    commit

--- src/AmazonCustomer.R
+++ src/AmazonCustomer.R
@@ -7,3 +7,12 @@ amazon <- read.csv("C:/Users/gavin/PycharmProjects/ConsumerBehaviour/src/data/Am
 
 # View the structure of the dataframe
 str(amazon) 
+
+# Summary statistics for numerical variables
+summary(amazon$age)  # Summary statistics for age
+
+# Frequency table for categorical variables
+table(amazon$gender)  # Frequency table for gender
+
+# Mean purchase frequency
+mean(amazon$Purchase_Frequency)  # Assuming Purchase_Frequency is encoded as numeric
