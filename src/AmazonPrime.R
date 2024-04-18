79f0381f6ffa3c374f619862abeedbed7774b0d0
GavDotZip <gavdotzip@gmail.com>
2024-04-18 21:40:19 +0100

    commit

---
+++ src/AmazonPrime.R
@@ -0,0 +1,9 @@
+# Dataset downloaded from https://www.kaggle.com/datasets/arnavsmayan/amazon-prime-userbase-dataset
+# Load the required library for data manipulation
+library(dplyr)
+
+# Read the CSV file into a dataframe
+prime <- read.csv("C:/Users/gavin/PycharmProjects/ConsumerBehaviour/src/data/amazon_prime_users.csv")
+
+# View the structure of the dataframe
+str(prime)
